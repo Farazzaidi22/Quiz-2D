4a1209c52815baa9cec043b8616af23ef6da7fdb
Faraz Khalid <farazkhalid05@yahoo.com>
2020-05-06 15:02:34 +0500

    Replaced gameobjects with images

---
+++ Quiz-game/Assets/Scripts/MoveWithButtons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveWithButtons : MonoBehaviour
+{
+    //Outside your functions where you want to set all your declarations...
+    public Sprite[] gallery; //store all your images in here at design time
+    public Image displayImage; //The current image thats visible
+    public Button nextImg; //Button to view next image
+    public Button prevImg; //Button to view previous image
+    public int i = 0; //Will control where in the array you are
+
+    public void BtnNext()
+    {
+        if (i + 1 < gallery.Length)
+        {
+            i++;
+        }
+    }
+
+    public void BtnPrev()
+    {
+        if (i - 1 > 0)
+        {
+            i--;
+        }
+    }
+
+    void Update()
+    {
+        displayImage.sprite = gallery[i];
+    }
+}
